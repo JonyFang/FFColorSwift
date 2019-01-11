f6ec7bb58e733530a30001822be82a787422622a
JonyFang <jony.chunfang@gmail.com>
2019-01-11 17:53:10 +0800

    Add mac target
    Signed-off-by: JonyFang <jony.chunfang@gmail.com>

---
+++ FFColorSwift-Mac/FFColorSwift_Mac.h
@@ -0,0 +1,19 @@
+//
+//  FFColorSwift_Mac.h
+//  FFColorSwift-Mac
+//
+//  Created by JonyFang on 2019/1/11.
+//  Copyright © 2019年 JonyFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FFColorSwift_Mac.
+FOUNDATION_EXPORT double FFColorSwift_MacVersionNumber;
+
+//! Project version string for FFColorSwift_Mac.
+FOUNDATION_EXPORT const unsigned char FFColorSwift_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFColorSwift_Mac/PublicHeader.h>
+
+
